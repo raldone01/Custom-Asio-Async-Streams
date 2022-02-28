791df83812bb64e21be3ae3780d4eea998a93875
raldone01 <raldone01@gmail.com>
2022-02-28 22:02:54 +0100

    Add a license notice to the individual source files.

--- src/IteratorConcepts.cpp
+++ src/IteratorConcepts.cpp
@@ -1,4 +1,10 @@
-/*
+/* Copyright 2022 The CustomAsioAsyncStreams Contributors.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * This file explores the new <iterator> concepts.
  * https://stackoverflow.com/questions/71135323/how-to-properly-constrain-an-iterator-based-function-using-concepts/71135490#71135490
  */
